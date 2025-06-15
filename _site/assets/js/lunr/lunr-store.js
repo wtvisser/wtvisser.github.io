bdd42bd6aef4248e0306c828941663b4d7ec58c8
Wicher Visser <wtvisser@gmail.com>
2025-06-15 18:00:50 +0000

    moved website files to root

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Personal Website and Blog",
+        "excerpt":"Welcome to my new personal web site and blog. Here, I plan to communicate updates about my activities and ponderings.   Feel free to browse around. Contact me via this page.  ","categories": ["website"],
+        "tags": [],
+        "url": "/website/2025/06/01/welcome-to-my-blog-and-website.html",
+        "teaser": null
+      }]
